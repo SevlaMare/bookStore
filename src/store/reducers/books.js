cbc11ec429920ca42603c1619c373013e94ab961
Abdel <juniorperezpy@gmail.com>
2020-09-28 18:04:54 -0400

    Add createBook and removeBook actions

--- src/store/reducers/books.js
+++ src/store/reducers/books.js
@@ -7,11 +7,11 @@ const INITIAL_STATE = {
   users: 'TEST123',
 };
 
-// const action1 = { type="CREATE_BOOK", book: {title, category, id}, index }
+const action1 = { type="CREATE_BOOK", book: {title, category, id}, index }
 
 const ReducerBooks = (state = INITIAL_STATE, action) => {
-  if (action.type == "CREATE_BOOK") return [...state, action.book]
-  if (action.type == "REMOVE_BOOK") return [...state.slice(0, index), ...state.slice(index + 1) ]
+  if (action.type === "CREATE_BOOK") return [...state, action.book];
+  if (action.type === "REMOVE_BOOK") return [...state.slice(0, index), ...state.slice(index + 1) ];
 
   return state;
 }
