9e08dbf05cb657f2218e7340aae260255ec87b8f
Mare <sevilamare@gmail.com>
2020-09-28 19:15:49 -0300

    litner fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ import './css/style.css';
 import { Provider } from 'react-redux';
 import store from './store/index';
 
+const INITIAL_STATE = {
+  books: [
+    { id: 1, title: 'Book 1', category: 'Action' },
+    { id: 2, title: 'Book 2', category: 'Biography' },
+    { id: 3, title: 'Book 3', category: 'Learning' },
+  ],
+};
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-, document.querySelector('#app'));
+  </Provider>,
+  document.querySelector('#app'),
+);
+
+export default INITIAL_STATE;
--- src/store/reducers/books.js
+++ src/store/reducers/books.js
@@ -1,16 +1,18 @@
-const INITIAL_STATE = {
-  books: [
-    { id: 1, title: 'Book 1', category: 'Action' },
-    { id: 2, title: 'Book 2', category: 'Biography' },
-    { id: 3, title: 'Book 3', category: 'Learning' },
-  ],
-};
+import { INITIAL_STATE } from '../../index';
+
+// const INITIAL_STATE = {
+//   books: [
+//     { id: 1, title: 'Book 1', category: 'Action' },
+//     { id: 2, title: 'Book 2', category: 'Biography' },
+//     { id: 3, title: 'Book 3', category: 'Learning' },
+//   ],
+// };
 
 const ReducerBooks = (state = INITIAL_STATE, action) => {
-  if (action.type === "CREATE_BOOK") return [...state, action.book];
-  if (action.type === "REMOVE_BOOK") return [...state.slice(0, index), ...state.slice(index + 1) ];
+  if (action.type === 'CREATE_BOOK') return [...state, action.book];
+  if (action.type === 'REMOVE_BOOK') return [...state.slice(0, index), ...state.slice(index + 1)];
 
   return state;
-}
+};
 
 export default ReducerBooks;
