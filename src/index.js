bb8cfa818e1b4befde9f9424b844691514bd8db1
Abdel <juniorperezpy@gmail.com>
2020-09-29 16:28:21 -0400

    Remove commented initial state object

--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ import rootReducer from './reducers/index';
 import App from './components/App';
 import './css/style.css';
 
-// const INITIAL_STATE = {
-//   books: [
-//     { id: 1, title: 'Book 1', category: 'Action' },
-//     { id: 2, title: 'Book 2', category: 'Biography' },
-//     { id: 3, title: 'Book 3', category: 'Learning' },
-//   ],
-// };
-
-// const store = createStore(combineReducers, INITIAL_STATE);
 const store = createStore(rootReducer);
 
 ReactDOM.render(
