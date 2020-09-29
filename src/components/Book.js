53ad5571e1b5fc2a351f641c6c7d0b10c7fb920c
Mare <sevilamare@gmail.com>
2020-09-29 12:16:13 -0300

    props for book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Book = props => {
   const { id, title, category } = props;
@@ -10,6 +11,16 @@ const Book = props => {
       <td>{category}</td>
     </tr>
   );
+};
+
+Book.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string.isRequired,
+  category: PropTypes.func.isRequired,
+};
+
+Book.defaultProps = {
+  id: Math.floor(),
 };
 
 export default Book;
