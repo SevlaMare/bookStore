7dfc67fa5e40b869692aa162f655e5fcc4f5f9ff
Abdel <juniorperezpy@gmail.com>
2020-09-29 17:13:02 -0400

    Add handler submit and handle change for books form

--- src/container/BooksForm.js
+++ src/container/BooksForm.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { createBook } from '../actions';
+
+import { useDispatch } from 'react-redux';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <form>
-    <label htmlFor="title">
-      Title:
-      <input type="text" id="title" name="title" />
-    </label>
+const BooksForm = ({ fetchedBooks }) => {
+  const dispatch = useDispatch();
+  const [newBook, setNewBook] = useState({ title: '', category: '' });
 
-    <label htmlFor="category">
-      Category:
-      <select type="text" id="category" name="category">
-        { categories.map(category => <option key={category}>{category}</option>)}
-      </select>
-    </label>
+  const handleChange = event => {
+    event.persist();
+    setNewBook(state => {
+      console.log(state);
+      return { ...state, [event.target.name]: event.target.value };
+    });
+  };
 
-    <button type="submit">Submit</button>
-  </form>
-);
+  const handleSubmit = () =>
+    dispatch(createBook(newBook));
 
-export default BooksForm;
+  return (
+    <form>
+      <label htmlFor="title">
+        Title:
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleChange}
+        />
+      </label>
+
+      <label htmlFor="category">
+        Category:
+        <select type="text" id="category" name="category" onChange={handleChange} >
+          { categories.map(category => <option key={category}>{category}</option>)}
+        </select>
+      </label>
+
+      <button
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+
+export default connect(state => ({
+  fetchedBooks: state.ReducerBooks.books,
+}))(BooksForm);
