3ad3a586192c4db55bcf6fb652081c9a9aea645f
Mare <sevilamare@gmail.com>
2020-10-01 12:21:47 -0300

    filter component v1

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <h2>Hi</h2>
+  )
+}
--- src/container/CategoryFilter.js
+++ src/container/CategoryFilter.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const CategoryFilter = ({ filter }) => {
-  console.log('ARRAY>>>>>>>', filter)
+import CATEGORIES from '../constants'
+
+const CategoryFilter = () => {
+  // console.log('ARRAY>>>>>>>', filter)
 
   return (
-    <form>
-      <input type="checkbox"/>
+    <form className='filter'>
+      { CATEGORIES.map(category => (
+        <label>{category}
+          <input type="checkbox" value={category}/>
+        </label>
+      ))}
     </form>
   )
 }
 
-export default connect(state => {
-  console.log('state>>>>>>>', state)
-  filter: state.FilterBooks.filter
-})(CategoryFilter);
+export default CategoryFilter;
+
+// export default connect(state => {
+//   console.log('state>>>>>>>', state)
+//   filter: state.FilterBooks.filter
+// })(CategoryFilter);
