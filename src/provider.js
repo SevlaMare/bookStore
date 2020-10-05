601de88edfa0fe58b0916f81784528e51406f43b
Abdel <juniorperezpy@gmail.com>
2020-10-05 18:13:41 -0300

    Add initial api provider structure

---
+++ src/provider.js
@@ -0,0 +1,26 @@
+const url = 'https://frozen-anchorage-06057.herokuapp.com/api/v1/books';
+
+const getBooks = async () => {
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+  });
+
+  return response.json();
+};
+
+const saveBook = async (title, category) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title, category }),
+  });
+
+  if (response.status === 400) { throw new Error('You need your name to send.'); }
+
+  return response.json();
+};
+
+export { sendScore, getScore };
